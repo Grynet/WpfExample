84b14ec9b28561b700ceb7f9eec029de88155c35
Fredrik Nystad <nystad.fredrik@gmail.com>
2025-11-27 19:30:34 +0100

    Created Model, View, Viewmodel folders

--- src/WpfExample.App/View/MainWindow.xaml.cs
+++ src/WpfExample.App/View/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfExample.App
+namespace WpfExample.App.View
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
