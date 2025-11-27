c7e102b76d5fa08efd32b8ffbf92f78128b6080d
Fredrik Nystad <nystad.fredrik@gmail.com>
2025-11-27 21:55:05 +0100

    Added ICustomerRepository

---
+++ src/WpfExample.App/Repositories/ICustomerRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using WpfExample.App.Model;
+
+namespace WpfExample.App.Repositories
+{
+    public interface ICustomerRepository
+    {
+        public Task<Customer> Get(int id, CancellationToken cancellationToken);
+        public Task<IEnumerable<Customer>> GetAll(CancellationToken cancellationToken);
+        public Task<Customer> AddOrUpdate(Customer customer);
+        public Task Delete(int id);
+    }
+}
