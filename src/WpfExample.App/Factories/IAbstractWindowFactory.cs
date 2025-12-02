f7a5967e4ce33a0ac4ae1a5a6d2e6f4430c9ee7b
Fredrik Nystad <nystad.fredrik@gmail.com>
2025-12-02 23:16:19 +0100

    IAbstractWindowFactory.Create() => CreateWindow()

--- src/WpfExample.App/Factories/IAbstractWindowFactory.cs
+++ src/WpfExample.App/Factories/IAbstractWindowFactory.cs
@@ -4,6 +4,6 @@ namespace WpfExample.App.Factories
 {
     public interface IAbstractWindowFactory<TWindow> where TWindow : Window
     {
-        TWindow Create();
+        TWindow CreateWindow();
     }
 }
