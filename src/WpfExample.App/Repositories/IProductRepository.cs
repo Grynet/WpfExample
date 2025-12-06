43656d7f60f51a1159a34ea97783d34e8a4a4c55
Fredrik Nystad <nystad.fredrik@gmail.com>
2025-12-06 23:16:09 +0100

    Added Product model & ProductRepository

---
+++ src/WpfExample.App/Repositories/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using WpfExample.App.Mvvm.Models;
+
+namespace WpfExample.App.Repositories
+{
+    public interface IProductRepository
+    {
+        public Task<Product> Get(Guid id, CancellationToken cancellationToken);
+        public Task<ICollection<Product>> GetAll(CancellationToken cancellationToken);
+        public Task AddOrUpdate(Product product);
+        public Task Delete(Guid id);
+    }
+}
