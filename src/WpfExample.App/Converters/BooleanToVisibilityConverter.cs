85758303782f6291ed95c6487241163e600f5bcc
Fredrik Nystad <nystad.fredrik@gmail.com>
2025-12-09 20:43:48 +0100

    BooleanToVisibilityConverter added ConvertBack() implementation

--- src/WpfExample.App/Converters/BooleanToVisibilityConverter.cs
+++ src/WpfExample.App/Converters/BooleanToVisibilityConverter.cs
@@ -24,7 +24,26 @@ namespace WpfExample.App.Converters
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            if (value is not Visibility)
+                throw new InvalidCastException($"{nameof(value)} wasn't of type {nameof(Visibility)}");
+
+            var visibility = (Visibility)value;
+
+            bool booleanValue;
+            switch (visibility)
+            {
+                case Visibility.Visible:
+                    booleanValue = true;
+                    break;
+                case Visibility.Collapsed:
+                case Visibility.Hidden:
+                    booleanValue= false;
+                    break;
+                default:
+                    throw new InvalidCastException($"The {nameof(Visibility)} value of {visibility} doesn't have a boolean mapping");
+            }
+
+            return Invert ? !booleanValue : booleanValue;
         }
     }
 }
